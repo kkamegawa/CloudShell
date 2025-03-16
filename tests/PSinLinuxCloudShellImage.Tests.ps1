a1fe583505aa9d459f40d47615e45f8f37b09ad9
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2025-03-16 13:51:08 +0900

    Update Node.JS version check to use wildcard matching

--- tests/PSinLinuxCloudShellImage.Tests.ps1
+++ tests/PSinLinuxCloudShellImage.Tests.ps1
@@ -18,7 +18,7 @@ Describe "Various programs installed with expected versions" {
 
     It "Static Versions" {
         # These programs are installed explicitly with specific versions
-        $script:pmap["Node.JS"].Version | Should -Be '20.*'
+        $script:pmap["Node.JS"].Version -like '20.*'
         $script:pmap["PowerShell"].Version | Should -BeLike '7.5*'
     }
 
